b0c03c9fd9d6cc313a7514fce81ecbd92cb68780
Unknown <olshovskiy@gmail.com>
2017-08-02 12:01:47 +0200

    Add CRUDController and template for list recipes

--- emeal/App_Start/RouteConfig.cs
+++ emeal/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace emeal
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CRUD", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ emeal/Controllers/CRUDController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace emeal.Controllers
+{
+    public class CRUDController : Controller
+    {
+        // GET: CRUD
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CRUD/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CRUD/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CRUD/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CRUD/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CRUD/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CRUD/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CRUD/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
