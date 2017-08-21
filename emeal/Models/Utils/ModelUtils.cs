2ebc94aaee26e84d54aa7d2fa793b6482b125c2b
Bartosz <elitarform@gmail.com>
2017-08-21 16:23:44 +0200

    Add ModelUtils

---
+++ emeal/Models/Utils/ModelUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace emeal.Models.Utils
+{
+    internal static class ModelUtils
+    {
+        internal static bool InRange(this int checkedInt, int rangeBottom, int rangeTop)
+        {
+            return checkedInt >= rangeBottom && checkedInt <= rangeTop;
+        }
+
+        private static bool CheckUrlValid(this string source)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(source, UriKind.Absolute, out uriResult)
+                   && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+        private static bool IsDifficultyEnum(Enum checkedEnum)
+        {
+            return Enum.IsDefined(typeof(Difficulty), checkedEnum);
+        }
+
+        internal static bool IsRequestValid(this Recipe recipe)
+        {
+            return recipe.PathToImage.CheckUrlValid() && IsDifficultyEnum(recipe.DifficultyLevel);
+        }
+    }
+}
