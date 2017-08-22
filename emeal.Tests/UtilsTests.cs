9f26fc787ad88e17c52573621a7d16e8b77f1c35
Krzysztof Jaroska <kjaroska@gmail.com>
2017-08-22 19:25:52 +0200

    Add UtilsTests

---
+++ emeal.Tests/UtilsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using emeal.Models;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using emeal.Models.Utils;
+
+namespace emeal.Tests
+{
+    [TestFixture]
+    internal class UtilsTests
+    {
+        [Test]
+        public void InRangeTest()
+        {
+            const int valueInRange = 5;
+            const int valueOutsideRange = 55;
+            const int rangeBottom = 1;
+            const int rangeTop = 5;
+
+            var trueResult = valueInRange.InRange(rangeBottom, rangeTop);
+            var falseResult = valueOutsideRange.InRange(rangeBottom, rangeTop);
+
+            Assert.AreEqual(true, trueResult);
+            Assert.AreEqual(false, falseResult);
+        }
+
+        [Test]
+        public void testCheckUrlValid()
+        {
+            var validSource = "https://stackoverflow.com/";
+            var inValidSource = "stackoverflow.com";
+
+            var trueResult = validSource.CheckUrlValid();
+            var falseResult = inValidSource.CheckUrlValid();
+
+            Assert.AreEqual(true, trueResult);
+            Assert.AreEqual(false, falseResult);
+        }
+
+    }
+}
+
