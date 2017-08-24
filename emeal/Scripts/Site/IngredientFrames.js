950d8d4bb09f09a82abb8d6ff67dec217b77f4c9
Bartosz <elitarform@gmail.com>
2017-08-24 19:18:02 +0200

    Modify Ingredient frame template

--- emeal/Scripts/Site/IngredientFrames.js
+++ emeal/Scripts/Site/IngredientFrames.js
@@ -1,15 +1,17 @@
 "use strict";
 $(function () {
-
-
     var ingredientFrameCount = 0;
 
     $("#add-ingredient-frame").click(function () {
         var ingredientFrameIdString = "Ingredients[" + ingredientFrameCount + "]";
         var ingredientFrameHTMLContent = "\
             <div class='col-md-3' id='" + ingredientFrameIdString + "'>\
-                <label for='Ingredients[" + ingredientFrameCount + "].Name'>Name</label>\
-                <input name='Ingredients[" + ingredientFrameCount + "]' type='text'/>\
+                <label for='Ingredients[" + ingredientFrameCount + "].Product'>Product</label>\
+                <input name='Ingredients[" + ingredientFrameCount + "].Product' type='text'/>\
+                <label for='Ingredients[" + ingredientFrameCount + "].Amount'>Amount</label>\
+                <input name='Ingredients[" + ingredientFrameCount + "].Amount' type='text'/>\
+                <label for='Ingredients[" + ingredientFrameCount + "].UnitType'>Unit type</label>\
+                <input name='Ingredients[" + ingredientFrameCount + "].UnitType' type='text'/>\
                 <button class='btn btn-danger delete-ingredient-frame'>Delete</button>\
             </div>";
 
