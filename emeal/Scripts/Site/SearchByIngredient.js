28e7c17f1d274eb0cb7fdc25f5af55de4de1c692
Bartosz <elitarform@gmail.com>
2017-08-28 10:22:03 +0200

    Quick style fix

--- emeal/Scripts/Site/SearchByIngredient.js
+++ emeal/Scripts/Site/SearchByIngredient.js
@@ -1,28 +1,21 @@
 ﻿"use strict";
-$(function() {
+$(function () {
 
-    $("#add").click(function() {
+    $("#add").click(function () {
 
-    //First things first, we need our text:
-    var text = document.getElementById("ingredientSearch").value; //.value gets input values
+        var text = document.getElementById("ingredientSearch").value; //.value gets input values
 
-    //Now construct a quick list element
-    var li = document.createElement("li");
-    var delButton = "<button class='btnDel btn btn-danger btn-xs'>x</button>";
-    li.innerHTML = "<div class=\"btn btn-success btn-sm\">" + text + delButton + "</div>";
+        var li = document.createElement("li");
+        var delButton = "<button class='btnDel btn btn-danger btn-xs'>x</button>";
+        li.innerHTML = delButton + "<div class='btn btn-success btn-sm'>" + text + "</div>";
 
-    //Now use appendChild and add it to the list!
-    document.getElementById("list").appendChild(li);
-    document.getElementById("ingredientSearch").value = "";
+        document.getElementById("list").appendChild(li);
+        document.getElementById("ingredientSearch").value = "";
     });
 
-    //used event delegation
-    $(document).on("click", ".btnDel", function(e) {
+    $(document).on("click", ".btnDel", function (e) {
         var ingredient = $(this).parent().parent();
         ingredient.remove();
     });
 
 });
-
-
-//<li><div class="btn btn-success btn-sm">afdsgdhf<div style="margin-left: 10%" class="btnDel btn btn-danger btn-xs">x</div></div></li>
