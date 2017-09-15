f928cab1b9128d19dee9d23162636241d34e3a41
Krzysztof Jaroska <kjaroska@gmail.com>
2017-09-15 13:56:16 +0200

    Add RecipeFinder interface and service

---
+++ emeal/Services/Interfaces/IRecipeFinder.cs
@@ -0,0 +1,15 @@
+﻿using emeal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emeal.Services
+{
+    public interface IRecipeFinder
+    {
+        List<int> FindRelevantRecipes(List<Recipe> allRecipes, List<int> queryArr);
+        List<int> GetProductsIdsFromRecipe(Recipe recipe);
+    }
+}
---
+++ emeal/Services/RecipeFinderService.cs
@@ -0,0 +1,61 @@
+﻿using emeal.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace emeal.Models.Utils
+{
+    public class RecipeFinderService : IRecipeFinder
+    {
+
+        public List<int> FindRelevantRecipes(List<Recipe> allRecipes, List<int> queryArr)
+        {
+            var recipeCoefficient = new List<Tuple<int, int>>();    // (recipeID, matchCoefficient)
+            var result = new List<int>();   // Coefficient: 0 bestMatch, 1 oneRedundantIngredient, -1 oneMissingIngredient
+            int maxProductNumberDifference = 5;
+
+            foreach (var recipe in allRecipes)
+            {
+                var productsIds = GetProductsIdsFromRecipe(recipe);
+                
+                var exceptQueryRecipe = queryArr.Except(productsIds).ToList().Count;
+                var exceptRecipeQuery = productsIds.Except(queryArr).ToList().Count;
+
+                int queryLen = queryArr.Count;
+                int recipeLen = productsIds.Count;
+
+                if(Math.Abs(queryLen - recipeLen)<= maxProductNumberDifference)
+                {
+                    if (exceptQueryRecipe == queryLen)
+                        continue;
+                    else if (exceptQueryRecipe >= 0 && exceptRecipeQuery > 0)
+                        recipeCoefficient.Add(Tuple.Create(recipe.Id, -exceptRecipeQuery + recipeLen * 10));
+                    else if (exceptQueryRecipe >= 0 && queryLen >= recipeLen)
+                        recipeCoefficient.Add(Tuple.Create(recipe.Id, exceptQueryRecipe));
+                    else if (exceptQueryRecipe == 0 && queryLen < recipeLen)
+                        recipeCoefficient.Add(Tuple.Create(recipe.Id, -exceptRecipeQuery + recipeLen * 10));
+                    else
+                        recipeCoefficient.Add(Tuple.Create(recipe.Id, -exceptRecipeQuery + recipeLen * 10));
+                }
+            }
+            recipeCoefficient.Sort((y, x) => Math.Abs(y.Item2).CompareTo(Math.Abs(x.Item2)));
+            
+            foreach (var tuple in recipeCoefficient)
+            {
+                result.Add(tuple.Item1);
+            }
+            return result;
+        }
+
+        public List<int> GetProductsIdsFromRecipe(Recipe recipe)
+        {
+            var productsIdsInRecipe = new List<int>();
+            foreach (var ingredient in recipe.Ingredients)
+            {
+                productsIdsInRecipe.Add(ingredient.Product.Id);
+            }
+            return productsIdsInRecipe;
+        }
+    }
+}
