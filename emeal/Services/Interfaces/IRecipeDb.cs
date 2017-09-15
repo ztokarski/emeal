c2920f5062062b5ba799615961974fe37c80287e
Krzysztof Jaroska <kjaroska@gmail.com>
2017-09-15 13:55:08 +0200

    Add IRecipeDb interface into services

---
+++ emeal/Services/Interfaces/IRecipeDb.cs
@@ -0,0 +1,14 @@
+﻿using emeal.Models;
+using System.Data.Entity;
+
+namespace emeal
+{
+    public interface IRecipeDb
+    {
+        DbSet<Ingredient> Ingredients { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<Recipe> Recipes { get; set; }
+        DbSet<Step> Steps { get; set; }
+        DbSet<User> Users { get; set; }
+    }
+}
