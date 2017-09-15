3239c4d1238baca9b7da59c4caf5367d918046a1
Krzysztof Jaroska <kjaroska@gmail.com>
2017-09-15 13:47:45 +0200

    Add Unity configurations and bootstrapper with DI

---
+++ emeal/Bootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Mvc;
+using emeal.Models.Utils;
+using emeal.Models;
+using emeal.Controllers;
+using emeal.Services;
+
+namespace emeal
+{
+    public class Bootstrapper
+    {
+        public static IUnityContainer Initialize()
+        {
+            var container = BuildUnityContainer();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+            return container;
+        }
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var container = new UnityContainer();
+
+            // register all your components with the container here  
+            //This is the important line to edit  
+            container.RegisterType<IRecipeFinder, RecipeFinderService>();
+            container.RegisterType<IRecipeDb, RecipeDb>();
+
+            RegisterTypes(container);
+            return container;
+        }
+        public static void RegisterTypes(IUnityContainer container)
+        {
+
+        }
+    }
+}
