5efc4dca41003d40dd137e341ece00be95a9388d
Krzysztof Jaroska <kjaroska@gmail.com>
2017-09-26 13:29:19 +0200

    Implement method from the Interface and cleancode/refactor

--- emeal/Services/RecipeFinderService.cs
+++ emeal/Services/RecipeFinderService.cs
@@ -2,22 +2,44 @@
 using emeal.Models;
 using emeal.Services.Interfaces;
 using emeal.Strategies.Interfaces;
+using System.Linq;
+using System;
 
 namespace emeal.Services
 {
     // ReSharper disable once ClassNeverInstantiated.Global
-    public class RecipeFinderService : IRecipeFinder
+    public class RecipeFinderService : BaseService, IRecipeFinder
     {
         private readonly IRecipeSearchStrategy _strategy;
 
-        public RecipeFinderService(IRecipeSearchStrategy strategy)
+        public RecipeFinderService(IRecipeSearchStrategy strategy, IRecipeDb db) : base(db)
         {
             _strategy = strategy;
         }
 
-        public List<int> FindRelevantRecipes(IEnumerable<Recipe> recipeList, List<int> queryArr)
+        public List<Recipe> GetQueryResult(List<int> queryArr)
         {
+            var foundRecipeIds = FindRelevantRecipesIds(queryArr);
+            var matchedRecipes = FindMatchedRecipes(foundRecipeIds);
+            var sortedRecipes = SortMatchedRecipes(matchedRecipes, foundRecipeIds);
+
+            return sortedRecipes;
+        }
+
+        public List<int> FindRelevantRecipesIds(List<int> queryArr)
+        {
+            IEnumerable<Recipe> recipeList = GetAllRecipes();
             return _strategy.GetRelevantRecipeIds(recipeList, queryArr);
+        }
+
+        private IEnumerable<Recipe> FindMatchedRecipes(List<int> foundRecipeIds)
+        {
+            return GetAllRecipes().Where(rcp => foundRecipeIds.Contains(rcp.Id)).ToList();
+        }
+
+        private List<Recipe> SortMatchedRecipes(IEnumerable<Recipe> matchedRecipes, List<int> foundRecipeIds)
+        {
+            return matchedRecipes.OrderBy(rcp => foundRecipeIds.IndexOf(rcp.Id)).ToList();
         }
     }
 }
