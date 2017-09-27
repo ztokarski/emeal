35b1172783051af0a1e0757a1204b2fa74a9879d
Bartosz <elitarform@gmail.com>
2017-09-27 11:22:25 +0200

    Fix TypeScript Compiler config, modify Scripts structure

---
+++ emeal/Scripts/Site/src/IngredientFrames.ts
@@ -0,0 +1,26 @@
+﻿const ingredientContainer: JQuery<HTMLElement> = $("#Ingredients");
+const ingredientWarning: JQuery<HTMLElement> = $("#ingredient-warning");
+ingredientWarning.hide();
+
+$("#add-ingredient-frame").click(event => {
+	event.preventDefault();
+
+	const frame: HTMLElement = document.createElement("div");
+	frame.innerText = "Loading...";
+
+	ingredientContainer.append(frame);
+	$(frame).load("/Ingredient/PartialIngredient",
+		function (response, status) {
+			if (status === "error") {
+				ingredientWarning.text("You smart, you loyal... but an error occured! Try again.");
+				ingredientWarning.show();
+				$(this).remove();
+			}
+		});
+});
+
+ingredientContainer.on("click", ".delete-ingredient-frame",
+	function (this: HTMLElement, event) {
+		event.preventDefault();
+		$(this).parent().remove();
+	});
---
+++ emeal/Scripts/Site/src/StepFrames.ts
@@ -0,0 +1,29 @@
+var stepContainer = $("#Steps");
+var stepWarning = $("#step-warning");
+
+stepWarning.hide();
+
+$("#add-step-frame").click(
+	function (event: JQuery.Event<EventTarget, null>) {
+		event.preventDefault();
+		stepWarning.hide(250);
+
+		const frame = document.createElement("div");
+		frame.innerText = "Loading...";
+
+		stepContainer.append(frame);
+		$(frame).load("/Step/PartialStep",
+			function (response: Response, status: string) {
+				if (status === "error") {
+					stepWarning.text("You smart, you loyal... but an error occured! Try again.");
+					stepWarning.show(250);
+					$(this).remove();
+				}
+			});
+	});
+
+stepContainer.on("click", ".delete-step-frame",
+	function (event: JQuery.Event<EventTarget, null>) {
+		event.preventDefault();
+		$(this).parent().remove();
+	});
