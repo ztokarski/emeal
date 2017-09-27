b136738bc95dc2fb27f81c6bf5606f7db2d88f64
Krzysztof Jaroska <kjaroska@gmail.com>
2017-09-27 17:30:13 +0200

    Change IsValid() condition

--- emeal/Models/Utils/ModelUtils.cs
+++ emeal/Models/Utils/ModelUtils.cs
@@ -22,7 +22,7 @@ namespace emeal.Models.Utils
 
         internal static bool IsValid(this Recipe recipe)
         {
-            return recipe is null;
+            return recipe != null;
         }
     }
 }
