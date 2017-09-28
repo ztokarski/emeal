5b1a23a85cd81a36c3c1366275239567a9199128
Krzysztof Jaroska <kjaroska@gmail.com>
2017-09-28 15:14:10 +0200

    Add allergies select2 to SearchByIngredient ts file

--- emeal/Scripts/Site/src/SearchByIngredient.ts
+++ emeal/Scripts/Site/src/SearchByIngredient.ts
@@ -1,31 +1,33 @@
-﻿$(function () {
-	var ingredientResults: JQuery<HTMLElement> = $("#ingredient-search-results");
+﻿$(() => {
+    var ingredientResults: JQuery<HTMLElement> = $("#ingredient-search-results");
 
-	$("#ingredient-search-input").select2({
-		placeholder: "Enter your ingredients here...",
-		tags: false
-	});
+    $("#ingredient-search-input").select2({
+        placeholder: "Enter your ingredients here...",
+        tags: false
+    });
 
-$("#my-allergies-input").select2({
-    placeholder: "This products will be excluded from the search...",
-    tags: false
-});
+    $("#my-allergies-input").select2({
+        placeholder: "This products will be excluded from the search...",
+        tags: false
+    });
 
-$("#ingredient-search-form").submit(function (this: JQuery<HTMLElement>, event: JQuery.Event<EventTarget, null>) {
-	event.preventDefault();
-	$("#ingredient-search-form").submit(function (this: JQuery<HTMLElement>, event: JQuery.Event<EventTarget, null>) {
-		event.preventDefault();
+    $("#ingredient-search-form").submit(function(this: JQuery<HTMLElement>, event: JQuery.Event<EventTarget, null>) {
+        event.preventDefault();
+        $("#ingredient-search-form").submit(
+            function(this: JQuery<HTMLElement>, event: JQuery.Event<EventTarget, null>) {
+                event.preventDefault();
 
-		$.ajax({
-			url: (<any>this).action,
-			type: "POST",
-			data: $(this).serialize(),
-			success: function (responseData: string) {
-				ingredientResults.html(responseData);
-			},
-			error: () => {
-				ingredientResults.html("<h3>An error occured. Please try again. :(</h3>");
-			}
-		});
-	});
+                $.ajax({
+                    url: (<any>this).action,
+                    type: "POST",
+                    data: $(this).serialize(),
+                    success: function(responseData: string) {
+                        ingredientResults.html(responseData);
+                    },
+                    error: () => {
+                        ingredientResults.html("<h3>An error occured. Please try again. :(</h3>");
+                    }
+                });
+            });
+    });
 });
