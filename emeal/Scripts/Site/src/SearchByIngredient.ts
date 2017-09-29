f36c56d0420c05dcd4f0bfcd05f58cff7fea10d8
Bartosz <elitarform@gmail.com>
2017-09-29 13:59:17 +0200

    /Ickerday/migrate-to-scss: Auto stash before merge of "feature/Ickerday/migrate-to-scss" and "origin/MyAllergies"

--- emeal/Scripts/Site/src/SearchByIngredient.ts
+++ emeal/Scripts/Site/src/SearchByIngredient.ts
@@ -1,5 +1,5 @@
-﻿$(() => {
-    var ingredientResults: JQuery<HTMLElement> = $("#ingredient-search-results");
+﻿$(function (): void {
+	var ingredientResults: JQuery<HTMLElement> = $("#ingredient-search-results");
 
     $("#ingredient-search-input").select2({
         placeholder: "Enter your ingredients here...",
@@ -10,24 +10,20 @@
         placeholder: "This products will be excluded from the search...",
         tags: false
     });
+	$("#ingredient-search-form").submit(
+		function (this: JQuery<HTMLElement>, event: JQuery.Event<EventTarget, null>): void {
+			event.preventDefault();
 
-    $("#ingredient-search-form").submit(function(this: JQuery<HTMLElement>, event: JQuery.Event<EventTarget, null>) {
-        event.preventDefault();
-        $("#ingredient-search-form").submit(
-            function(this: JQuery<HTMLElement>, event: JQuery.Event<EventTarget, null>) {
-                event.preventDefault();
-
-                $.ajax({
-                    url: (<any>this).action,
-                    type: "POST",
-                    data: $(this).serialize(),
-                    success: function(responseData: string) {
-                        ingredientResults.html(responseData);
-                    },
-                    error: () => {
-                        ingredientResults.html("<h3>An error occured. Please try again. :(</h3>");
-                    }
-                });
-            });
-    });
+			$.ajax({
+				url: (<any>this).action,
+				type: "POST",
+				data: $(this).serialize(),
+				success: function (responseData: string): void {
+					ingredientResults.html(responseData);
+				},
+				error: () => {
+					ingredientResults.html("<h3>An error occured. Please try again. :(</h3>");
+				}
+			});
+		});
 });
