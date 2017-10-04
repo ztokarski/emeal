ce338791582f919fec097fd721b5c3423c27b59b
Bartosz <elitarform@gmail.com>
2017-10-04 17:13:11 +0200

    Add FinderServiceTests

---
+++ emeal.Tests/StrategyTests/RecipeFinderServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace emeal.Tests.StrategyTests
+{
+    internal class RecipeFinderServiceTests
+    {
+        [Test]
+        public void ()
+        {
+            
+        }
+    }
+}
