5db22c2a6eb1d1b75d297d2294d8b2bf2681c733
Bartosz <elitarform@gmail.com>
2017-10-04 17:53:57 +0200

    Prepare mocks

--- emeal.Tests/ServiceTests/RecipeFinderServiceTests.cs
+++ emeal.Tests/ServiceTests/RecipeFinderServiceTests.cs
@@ -1,11 +1,26 @@
-﻿using NUnit.Framework;
-using TddEbook.TddToolkit;
+﻿using emeal.Services;
+using emeal.Services.Interfaces;
+using emeal.Strategies;
+using Moq;
+using NUnit.Framework;
 
 namespace emeal.Tests.ServiceTests
 {
     [TestFixture]
     internal class RecipeFinderServiceTests
     {
+        private Mock<IRecipeDb> _mockedDb;
+        private Mock<RecipeSearchStrategy> _mockedStrategy;
+        private RecipeFinderService _finderService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockedDb = new Mock<IRecipeDb>();
+            _mockedStrategy = new Mock<RecipeSearchStrategy>();
+            _finderService = new RecipeFinderService(_mockedStrategy.Object, _mockedDb.Object);
+        }
+
         [Test]
         public void FindsRelevantRecipeIds()
         {
@@ -15,7 +30,6 @@ namespace emeal.Tests.ServiceTests
         [Test]
         public void GetsQueryResult()
         {
-            
         }
 
         [Test]
@@ -26,7 +40,6 @@ namespace emeal.Tests.ServiceTests
         [Test]
         public void SortsMatchedRecipes()
         {
-            
         }
     }
 }
